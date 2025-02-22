2f7a1d9c75e53db82f3cabfc5e4470acb711a4d6
BugHunter-X <askzenai@gmail.com>
2025-02-22 12:30:43 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,28 @@
+```javascript
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ 
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
+
+```html
+<!-- index.html -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Tailwind CSS Bug</title>
+  <link href="/dist/output.css" rel="stylesheet">
+</head>
+<body>
+  <div class="bg-red-500 p-4">This should be red.</div>
+</body>
+</html>
+```
